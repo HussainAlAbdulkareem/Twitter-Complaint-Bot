3dfcc835eed9ebba943d223088e7b7443a175078
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-21 18:34:05 +0300

    Grabs the result of the internet speed test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+import time
 
 PROMISED_DOWN = 150
 PROMISED_UP = 10
@@ -18,7 +21,13 @@ class InternetSpeedTwitterBot:
 
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
-        self.driver.find_element(By.CLASS_NAME, "start-text").click()
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "start-text"))).click()
+        #Wait for the evaluation to be finished
+        time.sleep(45)
+        self.down = self.driver.find_element(By.CLASS_NAME, "download-speed").text
+        self.up = self.driver.find_element(By.CLASS_NAME, "upload-speed").text
+        return self.down, self.up
+
 
 bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()
+print(bot.get_internet_speed())
